7c52730c24e3e08a39d6924b18f5b32413bc33c4
trodrigu <trodriguez91@icloud.com>
2015-12-26 11:25:25 -0800

    add create/new/show actions

--- app/controllers/targets_controller.rb
+++ app/controllers/targets_controller.rb
@@ -1,5 +1,27 @@
-class TargetController < ApplicationController
+class TargetsController < ApplicationController
 
   def index
+  end
+
+  def create
+    @target = Target.new(target_params)
+    if @target.save
+      redirect_to @target
+    else
+      flash[:danger] = 'Something was incorrect'
+      render 'new'
+    end
+  end
+
+  def new
+    @target = Target.new
+  end
+
+  def show
+    @target = Target.find_by_id(params[:id])
+  end
+
+  def target_params
+    params.require(:target).permit(:name, :business)
   end
 end
