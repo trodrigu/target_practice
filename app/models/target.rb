0ecb316fe38535237903898a1f5b15c186ec7e48
trodrigu <trodriguez91@icloud.com>
2015-12-28 08:29:58 -0800

    remove make_domain

--- app/models/target.rb
+++ app/models/target.rb
@@ -1,5 +1,5 @@
 class Target < ActiveRecord::Base
-  has_many :cadences
-  validates :name, :business, presence: true
   validates_email_realness_of :email
+  has_many :cadences
+  validates :first_name, :last_name, :business, presence: true
 end
