d16c644cc8196edebda16da23fb05e17e8c3ebae
trodrigu <trodriguez91@icloud.com>
2015-12-28 08:32:18 -0800

    EmailPermutator service obj

---
+++ app/actions/email_permutator.rb
@@ -0,0 +1,49 @@
+class EmailPermutator
+  include ActiveRecord::Validations
+
+  CURRENT_CADENCES = Cadence.all
+
+  def initialize(params)
+    @email = params[:email]
+    @first_name = params[:first_name]
+    @fi = @first_name.first
+    @last_name = params[:last_name]
+    @middle_name = params[:middle_name]
+    @business = params[:business]
+    @domain = domain
+
+    if @middle_name
+      @mi = @middle_name.first
+    else
+      @mi = @first_name.first
+    end
+    @ln = @last_name.first
+    @business = params[:business]
+  end
+
+  def create_target
+    CURRENT_CADENCES.each do |c|
+      @email = eval('"' + c.permutation + '"') + @domain
+      target_params = {
+        email: @email.downcase,
+        first_name: @first_name,
+        last_name: @last_name,
+        business: @business,
+        domain: @domain
+      }
+      target = Target.new(target_params)
+      if target.valid?
+        return target
+        break
+      else
+        @email = nil
+      end
+    end
+  end
+  
+  def domain
+    #TODO: deal will non .com's
+    "@#{@business}.com"
+  end
+end
+
