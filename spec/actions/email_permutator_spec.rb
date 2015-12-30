fce05aed93f619605687ba5bf12cda8e70b23a4c
trodrigu <trodriguez91@icloud.com>
2015-12-30 08:51:10 -0800

    add test for no email found

--- spec/actions/email_permutator_spec.rb
+++ spec/actions/email_permutator_spec.rb
@@ -9,60 +9,21 @@ RSpec.describe EmailPermutator do
     }
   }
 
-  puts 'CREATED CADENCES'
   subject { EmailPermutator.new(target_params).create_target }
   it 'can find an email for a target' do
-
     cadences = Cadence.create([
-      { permutation: '%{fn}' },
-      { permutation: '%{ln}' },
-      { permutation: '%{fn}%{ln}' },
-      { permutation: '%{fn}.%{ln}' },
-      { permutation: '%{fi}%{ln}' },
-      { permutation: '%{fi}.%{ln}' },
-      { permutation: '%{fn}%{li}' },
-      { permutation: '%{fn}.%{li}' },
-      { permutation: '%{fi}%{li}' },
-      { permutation: '%{fi}.%{li}' },
-      { permutation: '%{ln}%{fn}' },
-      { permutation: '%{ln}.%{fn}' },
-      { permutation: '%{ln}%{fi}' },
-      { permutation: '%{ln}.%{fi}' },
-      { permutation: '%{li}%{fn}' },
-      { permutation: '%{li}.%{fn}' },
-      { permutation: '%{li}%{fi}' },
-      { permutation: '%{li}.%{fi}' },
-      { permutation: '%{fi}%{mi}%{ln}' },
-      { permutation: '%{fi}%{mi}.%{ln}' },
-      { permutation: '%{fn}%{mi}%{ln}' },
-      { permutation: '%{fn}.%{mi}.%{ln}' },
-      { permutation: '%{fn}%{mn}%{ln}' },
-      { permutation: '%{fn}.%{mn}.%{ln}' },
-      { permutation: '%{fn}-%{ln}' },
-      { permutation: '%{fi}-%{ln}' },
-      { permutation: '%{fn}-%{li}' },
-      { permutation: '%{fi}-%{li}' },
-      { permutation: '%{ln}-%{fn}' },
-      { permutation: '%{ln}-%{fi}' },
-      { permutation: '%{li}-%{fn}' },
-      { permutation: '%{li}-%{fi}' },
-      { permutation: '%{fi}%{mi}-%{ln}' },
-      { permutation: '%{fn}-%{mi}-%{ln}' },
-      { permutation: '%{fn}-%{mn}-%{ln}' },
-      { permutation: '%{fn}_%{ln}' },
-      { permutation: '%{fi}_%{ln}' },
-      { permutation: '%{fn}_%{li}' },
-      { permutation: '%{fi}_%{li}' },
-      { permutation: '%{ln}_%{fn}' },
-      { permutation: '%{ln}_%{fi}' },
-      { permutation: '%{li}_%{fn}' },
-      { permutation: '%{li}_%{fi}' },
-      { permutation: '%{fi}%{mi}_%{ln}' },
-      { permutation: '%{fn}_%{mi}_%{ln}' },
-      { permutation: '%{fn}_%{mn}_%{ln}' }
+      { permutation: '#{@first_name}' }
     ])
     target = subject
-    expect(target.email).to eq 'trodriguez@parallel6.com'
+    target.emails.each do |e|
+      expect(e.email).to include 'tommy@parallel6.com'
+    end
+  end
+
+  context 'when not valid' do
+    it 'does not create a target' do
+      expect { subject }.not_to change { Target.count }
+    end
   end
 end
 
