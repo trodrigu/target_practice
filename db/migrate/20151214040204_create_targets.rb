bb84117dacb0786577ec6ba4ceabfa68e3c17eaa
trodrigu <trodriguez91@icloud.com>
2015-12-30 08:50:45 -0800

    remove email creation in target mig

--- db/migrate/20151214040204_create_targets.rb
+++ db/migrate/20151214040204_create_targets.rb
@@ -2,7 +2,6 @@ class CreateTargets < ActiveRecord::Migration
   def change
     create_table :targets do |t|
       t.string :name
-      t.string :email
       t.string :business
 
       t.timestamps null: false
