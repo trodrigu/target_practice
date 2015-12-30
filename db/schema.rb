d3611de8a92fcd3947088c1961d2623faa1917ed
trodrigu <trodriguez91@icloud.com>
2015-12-30 08:50:56 -0800

    updated schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151228010434) do
+ActiveRecord::Schema.define(version: 20151230152622) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,11 +22,19 @@ ActiveRecord::Schema.define(version: 20151228010434) do
     t.datetime "updated_at",  null: false
   end
 
+  create_table "emails", force: :cascade do |t|
+    t.string   "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer  "target_id"
+  end
+
+  add_index "emails", ["target_id"], name: "index_emails_on_target_id", using: :btree
+
   create_table "targets", force: :cascade do |t|
     t.string   "business"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string   "email"
     t.string   "domain"
     t.string   "first_name"
     t.string   "last_name"
@@ -51,4 +59,5 @@ ActiveRecord::Schema.define(version: 20151228010434) do
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
+  add_foreign_key "emails", "targets"
 end
