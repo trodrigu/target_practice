2f1bfafb724b5c5804a0bbee0545403d38c175c6
trodrigu <trodriguez91@icloud.com>
2016-01-01 15:41:18 -0800

    add in symbol to add meth

--- app/models/target.rb
+++ app/models/target.rb
@@ -1,10 +1,10 @@
 class Target < ActiveRecord::Base
   has_many :cadences
-  has_many :emails
+  has_many :emails, dependent: :destroy
   validates :first_name, :last_name, :business, presence: true
   validate :generate_email
 
   def generate_email
-    errors.add("Must have email") if self.emails.blank?
+    errors.add(:email, "Email missing") if self.emails.blank?
   end
 end
